d9c919ee8001e709874f235266eaef422b2381f1
S1nnixD <98417123+S1nnixD@users.noreply.github.com>
2022-02-23 20:21:49 +0100

    Trying to commit changes
    Doesn't work anymore for some reason

--- INET/Test Data/INET Tests/INET Tests/CheckloginTest.cs
+++ INET/Test Data/INET Tests/INET Tests/CheckloginTest.cs
@@ -27,28 +27,33 @@ public class CheckloginTest {
     driver.Quit();
   }
   [Test]
-  public void checklogin() {
-    driver.Navigate().GoToUrl("https://www.inet.se/");
-    driver.Manage().Window.Size = new System.Drawing.Size(1920, 1040);
-    driver.FindElement(By.CssSelector(".user > .text-nowrap")).Click();
-    driver.FindElement(By.Id("login.email")).SendKeys("asdasd@test.com");
-    driver.FindElement(By.Id("password")).SendKeys("asdasd");
-    driver.FindElement(By.CssSelector("label > input")).Click();
-    driver.FindElement(By.CssSelector(".x-modal-action-button")).Click();
+  public void checklogin()
+  {
+        driver.Navigate().GoToUrl("https://www.inet.se/");
+        driver.Manage().Window.Size = new System.Drawing.Size(1920, 1040);
+        driver.FindElement(By.CssSelector(".user > .text-nowrap")).Click();
+        driver.FindElement(By.Id("login.email")).SendKeys("asdasd@test.com");
+        driver.FindElement(By.Id("password")).SendKeys("asdasd");
+        driver.FindElement(By.CssSelector("label > input")).Click();
+        driver.FindElement(By.CssSelector(".x-modal-action-button")).Click();
     {
-      string value = driver.FindElement(By.CssSelector("label > input")).GetAttribute("value");
-      Assert.That(value, Is.EqualTo("on"));
+        string value = driver.FindElement(By.CssSelector("label > input")).GetAttribute("value");
+        Assert.That(value, Is.EqualTo("on"));
     }
-    Assert.That(driver.FindElement(By.CssSelector("strong")).Text, Is.EqualTo("Felaktigt användarnamn eller lösenord."));
-    driver.Navigate().GoToUrl("https://www.inet.se/");
-    driver.FindElement(By.CssSelector(".user > .text-nowrap")).Click();
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        Assert.That(driver.FindElement(By.CssSelector(".alert")).Text, Is.EqualTo("Felaktigt användarnamn eller lösenord."));
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        driver.Navigate().GoToUrl("https://www.inet.se/");
+        driver.FindElement(By.CssSelector(".user > .text-nowrap")).Click();
     {
-      string value = driver.FindElement(By.XPath("//*[@id=\"inet\"]/div[8]/div/div/form/section/div/div/div[3]/div/label/input")).GetAttribute("value");
-      Assert.That(value, Is.EqualTo("on"));
+            string value = driver.FindElement(By.CssSelector("label > input")).GetAttribute("value");
+            Assert.That(value, Is.EqualTo("on"));
     }
-    driver.FindElement(By.CssSelector(".x-modal-action-button")).Click();
-    Assert.That(driver.FindElement(By.CssSelector(".t1cke9b5")).Text, Is.EqualTo("Logga in"));
-    driver.FindElement(By.CssSelector(".fa-times")).Click();
-    Assert.That(driver.FindElement(By.CssSelector(".user > .text-nowrap")).Text, Is.EqualTo("Min sida"));
+        driver.FindElement(By.CssSelector(".x-modal-action-button")).Click();
+        Assert.That(driver.FindElement(By.CssSelector(".t1cke9b5")).Text, Is.EqualTo("Logga in"));
+
+        driver.FindElement(By.CssSelector(".fa-times")).Click();
+        Assert.That(driver.FindElement(By.CssSelector(".user > .text-nowrap")).Text, Is.EqualTo("Min sida"));
   }
+
 }
