7db335f39528c2cf090ce066f691921a6594d3e2
S1nnixD <98417123+S1nnixD@users.noreply.github.com>
2022-02-28 19:01:25 +0100

    Update ExecTests.bat

--- ExecTests.bat
+++ ExecTests.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "INET/Test Data/INET Tests/INET Tests/bin/Debug/net6.0/INET Tests.dll"
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "INET/Test Data/INET Tests/INET Tests/bin/Debug/net6.0/INET Tests.dll" /ResultsDirectory:TestResults
